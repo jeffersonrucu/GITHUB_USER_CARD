d007458fa7423acf35fab06dfa70927a47d501d3
Jefferson Oliveira <jefferoidtex7@gmail.com>
2022-08-18 21:23:01 -0300

    Adiciona Base do Projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 import './App.css';
 
 function App() {
+  const userNameGithub = 'jeffersonrucu';
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    api
+      .get(`/users/${userNameGithub}`)
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <div className='card'>
+          <figure className='card__image'>
+            <img src={user?.avatar_url} alt="Imagem do usuario"/>
+          </figure>
+          
+          <div className='card__content'>
+            <h1 className='card__name'>{user?.name}</h1>
+            <p className='card__bio'>{user?.bio}</p>
+          </div>
+          <a className='card__link' href={user?.html_url}>GitHub</a>
+        </div>
+      </div>
+    </>
   );
 }
 
